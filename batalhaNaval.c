9cff0043f5ae16174c5a796ab99a644e10dac2e3
ludidudu <87877524+ludidudu@users.noreply.github.com>
2025-10-24 00:16:39 +0000

    Nível mestre

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -1,53 +1,60 @@
 #include <stdio.h>
 
 int main() {
-    int tabuleiro[10][10];
-    int navio1[3] = {3, 3, 3};
-    int navio2[3] = {3, 3, 3};
-    int navio3[3] = {3, 3, 3};
-    int navio4[3] = {3, 3, 3};
+    int tabuleiro[10][10] = {0};  
+    int centro = 2; 
 
-    for (int i = 0; i < 10; i++){
-        for (int j = 0; j < 10; j++){
-            tabuleiro[i][j] = 0;
+    int cone[5][5] = {0};
+    int cruz[5][5] = {0};
+    int octaedro[5][5] = {0};
+
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (j >= centro - i && j <= centro + i && i <= centro)
+                cone[i][j] = 1;
         }
     }
 
-    int linha1 = 2;
-    int coluna1 = 4;
-
-    int linha2 = 5;
-    int coluna2 = 7;
-
-    int linha3 = 0;
-    int coluna3 = 0;
-
-    int linha4 = 0;
-    int coluna4 = 9;
-
-    for (int j = 0; j < 3; j++){
-        tabuleiro[linha1][coluna1 + j] = navio1[j];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i == centro || j == centro)
+                cruz[i][j] = 1;
+        }
     }
 
-    for (int i = 0; i < 3; i++){
-        tabuleiro[linha2 + i][coluna2] = navio2[i];
+    for (int i = 0; i < 5; i++) {
+        for (int j = 0; j < 5; j++) {
+            if (i <= centro) {
+                if (j >= centro - i && j <= centro + i)
+                    octaedro[i][j] = 1;
+            } else {
+                if (j >= i - centro && j <= (4 - (i - centro)))
+                    octaedro[i][j] = 1;
+            }
+        }
     }
 
-    for (int k = 0; k < 3; k++){
-        tabuleiro[linha3 + k][coluna3 + k] = navio3[k];
-    }
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            if (cone[i][j] == 1)
+                tabuleiro[i][j] = 3;
 
-    for ( int l = 0; l < 3; l++){
-        tabuleiro[linha4 + l][coluna4 - l] = navio4[l];
-    }
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            if (cruz[i][j] == 1)
+                tabuleiro[i][j + 5] = 2;
 
-    printf("=== TABULEIRO BATALHA NAVAL ===\n\n");
+    for (int i = 0; i < 5; i++)
+        for (int j = 0; j < 5; j++)
+            if (octaedro[i][j] == 1)
+                tabuleiro[i + 5][j + 2] = 1;
 
     for (int i = 0; i < 10; i++) {
-        for (int j = 0; j < 10; j++){
+        for (int j = 0; j < 10; j++) {
             printf("%d ", tabuleiro[i][j]);
         }
         printf("\n");
     }
+
     return 0;
 }
